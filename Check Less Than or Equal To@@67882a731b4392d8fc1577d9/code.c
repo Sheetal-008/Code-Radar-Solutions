4a372136bbea2cf324b973e6bfa1cd6e0b37b05b
Sheetal Sharma <sheetalprofwork9992@gmail.com>
2025-01-27 11:15:34 +0530

    Solution to Check Less Than or Equal To

--- Check Less Than or Equal To@@67882a731b4392d8fc1577d9/code.c
+++ Check Less Than or Equal To@@67882a731b4392d8fc1577d9/code.c
@@ -2,10 +2,10 @@
 int main() {
     int a,b;
     if (a<=b){
-        print("True");
+        printf("True");
     }
     else{
-        print("False");
+        printf("False");
     }
 
     return 0;
